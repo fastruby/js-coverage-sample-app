893622923fae574460c69401b8695d380d46506f
Ariel Juodziukynas <arieljuod@gmail.com>
2021-10-06 17:46:33 -0300

    Initial Rails app with React and Typescript

---
+++ app/javascript/packs/hello_react.tsx
@@ -0,0 +1,25 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+interface Props {
+  name: string;
+}
+
+const Hello = ({ name = "David" }: Props) => {
+  const onClick = () => {
+    console.log("React Clicked");
+  };
+
+  return <div onClick={onClick}>Hello {name}!</div>;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <Hello name="React with TypeScript" />,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
